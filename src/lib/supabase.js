7d1242eb0508ddc961b85dac6f2e4df362741842
Joaquin Howard A. Falar <falarjoaquinhoward@gmail.com>
2025-12-06 21:16:51 +0800

    save my current progress

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -4,3 +4,7 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_API
 const supabaseAnonKey = process.env.NEXT_PUBLIC_PUBLIC_API_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
+
+
+
