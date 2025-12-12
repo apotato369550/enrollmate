67c158949b166d812bb883db97c9a0f3803f02d6
Joaquin Howard A. Falar <falarjoaquinhoward@gmail.com>
2025-12-12 10:05:58 +0800

    Update Next.j to 15.5.3 to fix security vulnerability

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,10 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
-
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_API
-const supabaseAnonKey = process.env.NEXT_PUBLIC_PUBLIC_API_KEY
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
-
-
-
-
+const supabaseKey = process.env.NEXT_PUBLIC_PUBLIC_API_KEY
+export const supabase = createClient(supabaseUrl, supabaseKey)
